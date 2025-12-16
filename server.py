10bb849aac0335024e07fd961ed7949317c2e4be
Aliza OConnor <alizaoconnor1@yahoo.com>
2025-12-16 14:51:59 -0500

    now it serves correctly

--- server.py
+++ server.py
@@ -7,9 +7,12 @@ app = Flask("Emotion Detection")
 @app.route("/emotionDetector")
 def emotion_getter():
     text_to_analyze = request.args.get('textToAnalyze')
-    my_json = json.loads(emotion_detector(text_to_analyze))
-    output = f"For the given statement, the system response is 'anger': {my_json['anger']}, 'disgust': {my_json['disgust']} \
-    , 'fear': {my_json['fear']}, 'joy': {my_json['joy']} and 'sadness': {my_json['sadness']}. The dominant emotion is \
+    my_json = emotion_detector(text_to_analyze)
+    output = f"For the given statement, the system response is \
+    'anger': {my_json['anger']}, \
+    'disgust': {my_json['disgust']} \
+    , 'fear': {my_json['fear']}, 'joy': {my_json['joy']} \
+    and 'sadness': {my_json['sadness']}. The dominant emotion is \
     {my_json['dominant_emotion']}."
     print(output)
     return output
@@ -18,6 +21,6 @@ def emotion_getter():
 def render_index_page():
     return render_template('index.html')
 
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
