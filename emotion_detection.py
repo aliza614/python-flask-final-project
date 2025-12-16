9e9b52fcefcb3e620cd703b29470e1e9ab3dcc06
Aliza OConnor <alizaoconnor1@yahoo.com>
2025-12-15 22:56:58 -0500

    formatting output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,7 +1,20 @@
-import requests
+import requests, json
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = input_json, headers = headers)
-    return response.text
+    output_json = json.loads(response.text)['emotionPredictions'][0]['emotion']
+    dominant_emotion = ''
+    dominant_emotion_score= -1
+    emotions = ['anger','disgust', 'fear', 'joy','sadness']
+    for emotion in emotions:
+        if dominant_emotion == '':
+            dominant_emotion = emotion
+            dominant_emotion_score = output_json[emotion]
+        elif dominant_emotion_score < output_json[emotion]:
+            dominant_emotion = emotion
+            dominant_emotion_score = output_json[emotion]
+
+    output_json['dominant_emotion'] = dominant_emotion_score
+    return output_json
