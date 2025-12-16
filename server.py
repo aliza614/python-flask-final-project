62f534ce0f856c4bb4b41e0067f6c324516ab24e
Aliza OConnor <alizaoconnor1@yahoo.com>
2025-12-16 15:22:58 -0500

    pylint fixes

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
+"""serves website"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
-import json
 
 app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def emotion_getter():
+    """
+    emotion getter
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     my_json = emotion_detector(text_to_analyze)
-    if my_json['dominant_emotion'] == None:
+    if my_json['dominant_emotion'] is None:
         return "Invalid text! Please try again!."
     output = f"For the given statement, the system response is \
     'anger': {my_json['anger']}, \
@@ -21,8 +24,10 @@ def emotion_getter():
 
 @app.route("/")
 def render_index_page():
+    """
+    website getter
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
-
